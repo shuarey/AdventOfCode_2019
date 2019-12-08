494ce73ed952300922dc7e15f7bd16202df80ded
Joshua Reynvaan <joshua.reynvaan@gmail.com>
2019-12-07 23:18:52 -0800

    IntcodeComputer updates

--- Day 2 and 5/IntcodeComputer.cs
+++ Day 2 and 5/IntcodeComputer.cs
@@ -9,16 +9,25 @@ namespace AdventOfCode_2019
     public class IntcodeComputer
     {
         private int[] OpCodes { get; set; }
-        private const string path = @"C:\Users\joshu\source\repos\AdventOfCode_2019\Resources\opCodes_day";
+        public int Input { get; set; }
+        public List<int> diagnosticCodes = new List<int>();
+        public int LastCode { get; set; }
+        public char Mode1 { get; set; } = '0';
+        public char Mode2 { get; set; } = '0';
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+
+        private const string path = @"C:\Users\joshu\source\repos\AdventOfCode_2019\Resources\opCodes_";
 
         public enum DaysOfChallenge { day2 = 2, day5 = 5 }
 
-        public IntcodeComputer(DaysOfChallenge day)
+        public IntcodeComputer(DaysOfChallenge day, int input)
         {
+            Input = input;
             string path = BuildPath(day);
             string csv = File.ReadAllText(path);
-            string[] opCodeStrings = csv.Split(',');
-            OpCodes = Array.ConvertAll(opCodeStrings, int.Parse);
+            OpCodes = Array.ConvertAll(csv.Split(','), int.Parse);
+            Compute();
         }
 
         private string BuildPath(DaysOfChallenge day)
@@ -30,44 +39,82 @@ namespace AdventOfCode_2019
             return sb.ToString();
         }
 
-        public int GetFirstPosition()
-        {
-            Compute(OpCodes);
-            return OpCodes[0];
-        }
-
-        public void SetNounAndVerb(int noun, int verb)
-        {
-            OpCodes[1] = noun;
-            OpCodes[2] = verb;
-        }
-
-        private void Compute(int[] opCodes)
+        private void Compute()
         {
             int jump = 0;
-            for (int i = 0; i < opCodes.Length; i += jump + 1)
+            for (int i = 0; i < OpCodes.Length; i += jump + 1)
             {
-                if (opCodes[i] == 99)
+                if (ShouldTerminate(OpCodes[i].ToString())) { break; }
+                int newPosition = 0;
+                bool needsToJump = ShouldJump(OpCodes[i].ToString(), i, out newPosition);
+                if (needsToJump)
                 {
-                    return;
+                    i = newPosition;
+                    ResetModes();
+                    jump = -1;
+                    continue;
                 }
-                Mutate(opCodes[i], i, out jump);
+                Mutate(OpCodes[i].ToString(), i, out jump);
+                ResetModes();
             }
         }
 
-        private void Mutate(int opCode, int currentPos, out int paramCount)
+        private bool ShouldJump(string opCode, int currentPosition, out int position)
         {
-            var pos1 = OpCodes[currentPos + 1];
-            var pos2 = OpCodes[currentPos + 2];
-            var pos3 = OpCodes[currentPos + 3];
-            switch (opCode)
+            int code = 0;
+            SetModes(opCode);
+            SetValues(currentPosition);
+            position = currentPosition;
+            code = ParseCode(opCode);
+            if (code == 5 && Value1 != 0)
+            {
+                position = Value2;
+                return true;
+            }
+            if (code == 6 && Value1 == 0)
+            {
+                position = Value2;
+                return true;
+            }
+            return false;
+        }
+
+        private void Mutate(string opCode, int currentPos, out int paramCount)
+        {
+            int code = 0;
+
+            SetModes(opCode);
+            SetValues(currentPos);
+            code = ParseCode(opCode);
+
+            var positionToMutate = OpCodes[currentPos + 3];
+
+            switch (code)
             {
                 case 1:
-                    OpCodes[pos3] = OpCodes[pos1] + OpCodes[pos2];
+                    OpCodes[positionToMutate] = Value1 + Value2;
                     paramCount = 3;
                     break;
                 case 2:
-                    OpCodes[pos3] = OpCodes[pos1] * OpCodes[pos2];
+                    OpCodes[positionToMutate] = Value1 * Value2;
+                    paramCount = 3;
+                    break;
+                case 3:
+                    positionToMutate = GetPosition(Mode1, currentPos + 1);
+                    OpCodes[positionToMutate] = Input;
+                    paramCount = 1;
+                    break;
+                case 4:
+                    positionToMutate = GetPosition(Mode1, currentPos + 1);
+                    diagnosticCodes.Add(OpCodes[Value1]);
+                    paramCount = 1;
+                    break;
+                case 7:
+                    OpCodes[positionToMutate] = Value1 < Value2 ? 1 : 0;
+                    paramCount = 3;
+                    break;
+                case 8:
+                    OpCodes[positionToMutate] = Value1 == Value2 ? 1 : 0;
                     paramCount = 3;
                     break;
                 default:
@@ -75,5 +122,42 @@ namespace AdventOfCode_2019
                     break;
             }
         }
+
+        private void SetValues(int currentPosition)
+        {
+            Value1 = SetValue(Mode1, OpCodes[currentPosition + 1]);
+            Value2 = SetValue(Mode2, OpCodes[currentPosition + 2]);
+        }
+
+        private int SetValue(char mode, int param) => mode == '0' ? OpCodes[param] : param;
+
+        private void SetModes(string opCode)
+        {
+            if (opCode.Length == 3)
+            {
+                Mode1 = opCode[0];
+            }
+            else if (opCode.Length == 4)
+            {
+                Mode1 = opCode[1];
+                Mode2 = opCode[0];
+            }
+        }
+
+        private void ResetModes() { Mode1 = '0'; Mode2 = '0'; }
+
+        private int GetPosition(char mode, int currentPosition) => mode == '0' ? 
+            OpCodes[currentPosition] : currentPosition;
+
+        private int ParseCode(string opCode) => opCode.Length > 1 ?
+            Int32.Parse(opCode.Substring(opCode.Length - 2)) : Int32.Parse(opCode);
+
+        private bool ShouldTerminate(string opCode) => ParseCode(opCode) == 99 ? true : false;
+
+        public int GetFirstPosition() => OpCodes[0];
+
+        public int GetDiagnosticCode() => diagnosticCodes.Last();
+
+        public void SetNounAndVerb(int noun, int verb) { OpCodes[1] = noun; OpCodes[2] = verb; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,8 +7,10 @@ namespace AdventOfCode_2019
         
         static void Main(string[] args)
         {
-            IntcodeComputer computer = new IntcodeComputer(IntcodeComputer.DaysOfChallenge.day5);
-
+            int input = 5;
+            IntcodeComputer computer = new IntcodeComputer(IntcodeComputer.DaysOfChallenge.day5, input);
+            Console.WriteLine($"First position: {computer.GetFirstPosition()}");
+            Console.WriteLine($"Diagnostic code: {computer.GetDiagnosticCode()}");
         }
 
         //public int[] cellCompete(int[] states, int days)
